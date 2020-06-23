1cf5f4a14756bb6ba03a2e5e4cf1309c61dc4cc8
brichardsonfs <brichardson.fs@gmail.com>
2020-06-22 18:50:19 -0700

    Amended with GLMM

--- grw_climate_NA/for_Git/grw_final.R
+++ grw_climate_NA/for_Git/grw_final.R
@@ -1,9 +1,8 @@
-require(lme4)
-require(lmerTest)
+require(glmmTMB)
 require(ggplot2)
 require(doBy)
-require(MuMIn)
 require(broom)
+require(broomExtra)
 require(data.table)
 require(sjPlot)
 require(e1071)
@@ -11,27 +10,21 @@ require(insight)
 require(dplyr)
 require(lattice)
 require(Hmisc)
+require(DHARMa)
 #=================================================================================================#
 ###READ IN DATA
 grow1 <- read.csv(file="master6-15_grw_climNA.csv", sep=",",head=TRUE, na.string="na")
 #OMIT NAs
 grw <- na.omit(grow1)
 
-#CREATE LOG-TRANSFORMED GROWTH DATA
+#DISTRIBUTION
+hist(grw$grwvol_m,breaks = 200)
 skewness(grw$grwvol_m)
-grw_log = log1p(grw$grwvol_m)
-grw <- cbind(grw,grw_log)
 
-
-#SUM ACROSS YEARS (2011 to 2012)
-#grw1yr <- summaryBy(grwvol_m+grw_log ~ pop + ssp + ploidy + family + garden+MAT+MWMT+MCMT+
-#                      TD+MAP+MSP+AHM+SHM+DD0+DD5+DDL18+DDG18+NFFD+bFFP+eFFP+FFP+PAS+EMT+EXT+
-#                      MAR+Eref+CMD+RH+PPT_wt+PPT_sp+PPT_sm+PPT_at+DD0_wt+DD5_sm,data = grw,FUN = c(sum))
-#setnames(grw1yr, old = c('grwvol_m.sum','grw_log.sum'),new = c('grwvol_m','grw_log'))
 #SUMMARIZE 
-exp_sum1 <- summaryBy(pop+grwvol_m~pop+ssp+ploidy+garden, data = grw,FUN=c(length,mean,sd))
+exp_sum1 <- summaryBy(grwvol_m~pop+ssp+ploidy+garden, data = grw,FUN=c(length,mean,sd))
 exp_sum2 <- summaryBy(grwvol_m~ssp+garden, data = grw,FUN=c(mean,min,max,sd))
-exp_sum3 <- summaryBy(pop+grwvol_m~pop+ssp+ploidy+garden+year, data = grw,FUN=c(length,mean,sd))
+exp_sum3 <- summaryBy(grwvol_m~pop+ssp+ploidy+garden+year, data = grw,FUN=c(length,mean,sd))
 
 #APPENDIX S2 = exp_sum2
 #write.csv(exp_sum1,file = "grw_exp_summary1.csv")
@@ -51,49 +44,69 @@ ss + geom_bar(stat = "summary", fun.y = "mean")+facet_grid(.~garden)+ xlab("subs
 
 #=================================================================================================#
 ##SUMMARY BY POPULATION FOR ASSESSING CORRELATION WITH CLIMATE VARS
-popgrw <- summaryBy(grw_log+MAT+MWMT+MCMT+TD+MAP+MSP+AHM+SHM+DD0+DD5+DDL18+DDG18+NFFD+bFFP+eFFP+FFP+
+popgrw <- summaryBy(grwvol_m+MAT+MWMT+MCMT+TD+MAP+MSP+AHM+SHM+DD0+DD5+DDL18+DDG18+NFFD+bFFP+eFFP+FFP+
                       PAS+EMT+EXT+MAR+Eref+CMD+RH+PPT_wt+PPT_sp+PPT_sm+PPT_at+DD0_wt+DD5_sm ~ pop+garden+ssp, FUN = c(mean), data=grw)
-setnames(popgrw, old = c('grw_log.mean','MAT.mean','MWMT.mean','MCMT.mean','TD.mean','MAP.mean','MSP.mean','AHM.mean','SHM.mean','DD0.mean','DD5.mean','DDL18.mean','DDG18.mean','NFFD.mean','bFFP.mean','eFFP.mean','FFP.mean','PAS.mean','EMT.mean','EXT.mean','MAR.mean','Eref.mean','CMD.mean','RH.mean','PPT_wt.mean','PPT_sp.mean','PPT_sm.mean','PPT_at.mean','DD0_wt.mean','DD5_sm.mean'),
-         new = c('grw_log','MAT','MWMT','MCMT','TD','MAP','MSP','AHM','SHM','DD0','DD5','DDL18','DDG18','NFFD','bFFP','eFFP','FFP','PAS','EMT','EXT','MAR','Eref','CMD','RH','PPT_wt','PPT_sp','PPT_sm','PPT_at','DD0_wt','DD5_sm'))
+setnames(popgrw, old = c('grwvol_m.mean','MAT.mean','MWMT.mean','MCMT.mean','TD.mean','MAP.mean','MSP.mean','AHM.mean','SHM.mean','DD0.mean','DD5.mean','DDL18.mean','DDG18.mean','NFFD.mean','bFFP.mean','eFFP.mean','FFP.mean','PAS.mean','EMT.mean','EXT.mean','MAR.mean','Eref.mean','CMD.mean','RH.mean','PPT_wt.mean','PPT_sp.mean','PPT_sm.mean','PPT_at.mean','DD0_wt.mean','DD5_sm.mean'),
+         new = c('grwvol_m','MAT','MWMT','MCMT','TD','MAP','MSP','AHM','SHM','DD0','DD5','DDL18','DDG18','NFFD','bFFP','eFFP','FFP','PAS','EMT','EXT','MAR','Eref','CMD','RH','PPT_wt','PPT_sp','PPT_sm','PPT_at','DD0_wt','DD5_sm'))
 
-popgrw_sum <- summaryBy(grw_log+MAT+MWMT+MCMT+TD+MAP+MSP+AHM+SHM+DD0+DD5+DDL18+DDG18+NFFD+bFFP+eFFP+FFP+
+popgrw_sum <- summaryBy(grwvol_m+MAT+MWMT+MCMT+TD+MAP+MSP+AHM+SHM+DD0+DD5+DDL18+DDG18+NFFD+bFFP+eFFP+FFP+
                           PAS+EMT+EXT+MAR+Eref+CMD+RH+PPT_wt+PPT_sp+PPT_sm+PPT_at+DD0_wt+DD5_sm ~ pop+garden+ssp, FUN = c(mean), data=grw)
 
 #write.csv(x = popgrw_sum,file = "data_pop.csv")
 
-data_pop=with(popgrw, data.frame(MAT,MWMT,MCMT,TD,MAP,MSP,AHM,SHM,DD0,DD5,DDL18,DDG18,NFFD,bFFP,eFFP,FFP,PAS,EMT,EXT,MAR,Eref,CMD,RH,PPT_wt,PPT_sp,PPT_sm,PPT_at,DD0_wt,DD5_sm,grw_log)) 
+data_pop=with(popgrw, data.frame(MAT,MWMT,MCMT,TD,MAP,MSP,AHM,SHM,DD0,DD5,DDL18,DDG18,NFFD,bFFP,eFFP,FFP,PAS,EMT,EXT,MAR,Eref,CMD,RH,PPT_wt,PPT_sp,PPT_sm,PPT_at,DD0_wt,DD5_sm,grwvol_m)) 
 cor <- cor(data_pop)
 
-####VARS CHOSEN r > 0.3 = MCMT, DD0, DDL18, PAS, MAT
-##MCMT AND PAS HIGHEST NON COLINEAR VARS
-
 #=================================================================================================#
-###LMER
-grw1 <- lmer(grw_log ~ ssp + PAS + (1|garden:year) + (1|pop:(garden:year)),REML=TRUE, data=grw)
-grw2 <- lmer(grw_log ~ ssp + MCMT + (1|garden:year) + (1|pop:(garden:year)),REML=TRUE , data=grw)
-grw3 <- lmer(grw_log ~ ssp + MCMT + PPT_sm + (1|garden:year) + (1|pop:(garden:year)),REML=TRUE , data=grw)
-grw_wo_clim <- lmer(grw_log ~ ssp + (1|garden:year) + (1|pop:(garden:year)),REML=TRUE, data=grw)
+#GLMM
+mgrw1 <- glmmTMB(
+  grwvol_m ~ ssp + MCMT + PPT_sm + (1 | garden:year) + (1|pop:(garden:year)), 
+  ziformula = ~1, 
+  family = tweedie(), 
+  data = grw)
+mgrw2 <- glmmTMB(
+  grwvol_m ~ ssp + PAS + (1 | garden:year) + (1|pop:(garden:year)), 
+  ziformula = ~1, 
+  family = tweedie(), 
+  data = grw)
+mgrw3 <- glmmTMB(
+  grwvol_m ~ ssp + (1 | garden:year) + (1|pop:(garden:year)), 
+  ziformula = ~1, 
+  family = tweedie(), 
+  data = grw)
 
-##MODEL SELECTION WITH LRT
-anova(grw1,grw2,grw3,grw_wo_clim)
+#LRT for two models
+anova(mgrw1,mgrw2,mgrw3)
 
-### RESULTS
-summary(grw1)
-ranova(grw1)
-r.squaredGLMM(grw1)
-r.squaredGLMM(grw_wo_clim)
-grw1
-#Change to lmerMod for tables (Table 2)
-class(grw1) <- "lmerMod"
-tab_model(grw1,p.val = "kr", show.icc = FALSE)
+#BEST MODEL = mgrw2
+#MODEL RESULTS
+summary(mgrw2)
+#With climate
+tab_model(mgrw2,p.val = "wald", show.icc = FALSE,collapse.ci = TRUE)
+#Without climate
+tab_model(mgrw3,p.val = "wald", show.icc = FALSE,collapse.ci = TRUE)
+###EXAMINE RESIDUALS
+residuals <- resid(mgrw2) 
+summary(residuals)
+hist(residuals)
+plot(residuals)
+testUniformity(mgrw2)
 
-##LMER OUTPUT and MAKE DATAFRAME
-grw2_output <- augment(grw1)
-grw2_output <- merge(grw2_output,grw)
-grw2_summary <- summaryBy(grw_log+grwvol_m+.fitted+.fixed+PAS~
+
+#GARDEN AND YEAR EFFECTS (SJPLOT)
+#FigS1
+plot_model(mgrw2,type="re")
+
+##MAKE DATAFRAME
+grw2_output <- augment(mgrw2)
+#CHANGE TO DATAFRAME
+grw2_output <- as.data.frame(grw2_output)
+#grw2_output <- merge(grw2_output,grw)
+grw2_summary <- summaryBy(grwvol_m+.fitted+PAS~
                             pop+ssp+garden+year,data=grw2_output,FUN = c(mean))
-setnames(grw2_summary, old = c('grw_log.mean','grwvol_m.mean','.fitted.mean','.fixed.mean','PAS.mean'),
-         new = c('grw_log','grwvol_m','.fitted','.fixed','PAS'))
+setnames(grw2_summary, old = c('grwvol_m.mean','.fitted.mean','PAS.mean'),
+         new = c('grwvol_m','.fitted','PAS'))
+
 #write.csv(grw2_summary,file = "grw_lmer_output.csv")
 
 ##MODEL FIT
@@ -103,11 +116,6 @@ setnames(grw2_summary, old = c('grw_log.mean','grwvol_m.mean','.fitted.mean','.f
 #  scale_shape_manual(values=c(20,23))
 #cor.test(formula=~grw_log+.fixed,data = grw2_summary)
 
-###EXAMINE RESIDUALS
-residuals <- resid(grw1) 
-summary(residuals)
-hist(residuals)
-plot(residuals)
 
 
 ###SEED-GROWTH COMPARISON
@@ -141,32 +149,27 @@ summary(lmObserved)
 tab_model(lmObserved)
 
 ###EXTRACT RANDOM EFFECTS
-re_pop <- ranef(grw1,condVar=TRUE, whichel = "pop:(garden:year)")
-re_env <- ranef(grw1,condVar=TRUE, whichel = "garden:year")
-#Fig.S1
-dotplot(re_env)
-#DELIST to EXAMINE POP INTERACTION
-re_pop1 <- unlist(re_pop)
-re_pop2 <- as.vector(re_pop1)
+re1 <- ranef(mgrw2)
+popXgard <- (re1$cond$'pop:(garden:year)')
 
 ###EXAMINE GXE
 #GROWTH (Fig.2a)
 mm <- ggplot(exp_sum1, aes(y=grwvol_m.mean,x=garden,group=pop))
 mm + geom_point()+ geom_line(alpha=0.4)+facet_grid(.~ssp) + 
-  ylab(bquote('growth'~(m^3))) + theme_bw(base_size = 15)#+geom_text(aes(label=pop))
+  ylab(bquote('growth'~(m^3^-y))) + theme_bw(base_size = 15)#+geom_text(aes(label=pop))
 
 ###SUMMARIZE FOR GxE
 #BIND GxE RANDOM EFFECTS and SURVIVAL
-grw2_summary_re <- cbind(grw2_summary,re_pop2)
+grw2_summary_re <- cbind(grw2_summary,popXgard)
 
 #AVG YEAR
-grw2_summary_re <- summaryBy(grw_log+grwvol_m+.fitted+.fixed+PAS
-                          +re_pop2~pop+ssp+garden,data=grw2_summary_re,FUN = c(mean))
+grw2_summary_re <- summaryBy(grwvol_m+.fitted+PAS
+                          +popXgard~pop+ssp+garden,data=grw2_summary_re,FUN = c(mean))
 
 #Fig 4 Relationship between PAS and growth at each garden
-ww <- ggplot(grw2_summary_re, aes(PAS.mean,grw_log.mean,color=ssp,shape=ssp))
+ww <- ggplot(grw2_summary_re, aes(PAS.mean,grwvol_m.mean,color=ssp,shape=ssp))
 ww+ geom_point()+stat_smooth(method=lm,se=FALSE,linetype=4,fill=NA)+theme_bw(base_size = 15)+
-  xlab("Precipitation as Snow")+ylab("log(Growth)")+facet_grid(.~garden)
+  labs(x="Precipitation as snow", y=(bquote('Growth' ~(m^3)))) + facet_grid(.~garden)
 
 #SUMMARIZE TO GARDEN EFFECTS 
 Ogrw_GxE <- dplyr::filter(grw2_summary_re, garden!="Ephraim",garden!="Majors")
@@ -174,12 +177,12 @@ Mgrw_GxE <- dplyr::filter(grw2_summary_re, garden!="Ephraim",garden!="Orchard")
 Egrw_GxE <- dplyr::filter(grw2_summary_re, garden!="Majors",garden!="Orchard")
 
 #GARDEN-SSP CORRELATION
-with(subset(Ogrw_GxE, ssp==c("tridentata")),cor.test(PAS.mean, grw_log.mean))
-with(subset(Ogrw_GxE, ssp==c("wyomingensis")),cor.test(PAS.mean, grw_log.mean))
-with(subset(Mgrw_GxE, ssp==c("tridentata")),cor.test(PAS.mean, grw_log.mean))
-with(subset(Mgrw_GxE, ssp==c("wyomingensis")),cor.test(PAS.mean, grw_log.mean))
-with(subset(Egrw_GxE, ssp==c("tridentata")),cor.test(PAS.mean, grw_log.mean))
-with(subset(Egrw_GxE, ssp==c("wyomingensis")),cor.test(PAS.mean, grw_log.mean))
+with(subset(Ogrw_GxE, ssp==c("tridentata")),cor.test(PAS.mean.mean, grwvol_m.mean.mean))
+with(subset(Ogrw_GxE, ssp==c("wyomingensis")),cor.test(PAS.mean.mean, grwvol_m.mean.mean))
+with(subset(Mgrw_GxE, ssp==c("tridentata")),cor.test(PAS.mean.mean, grwvol_m.mean.mean))
+with(subset(Mgrw_GxE, ssp==c("wyomingensis")),cor.test(PAS.mean.mean, grwvol_m.mean.mean))
+with(subset(Egrw_GxE, ssp==c("tridentata")),cor.test(PAS.mean.mean, grwvol_m.mean.mean))
+with(subset(Egrw_GxE, ssp==c("wyomingensis")),cor.test(PAS.mean.mean, grwvol_m.mean.mean))
 
 #SEED YIELD POP X GARDEN INTERACTIONS (Fig.2b)
 nn <- ggplot(comp, aes(y=weight,x=garden,group=pop))
